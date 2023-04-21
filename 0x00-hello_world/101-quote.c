4fa95acb984a57d8d32f4f7e66e2115429878486
Clara4555 <favourloyalty8@gmail.com>
2023-04-20 17:19:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  *main - entry point
-  *Return: always 0 (success)
+  *main - Entry point
+  *Return: always 1 (Success)
   */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (0);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 	
