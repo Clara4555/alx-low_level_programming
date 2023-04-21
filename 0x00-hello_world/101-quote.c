1aefabc60a87b47876bd7a19531b3c93eb26e0f7
Clara4555 <favourloyalty8@gmail.com>
2023-04-20 17:13:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  *main - A c program that prints a line and stndard error
-  *Return 1 (Success)
+  *main - entry point
+  *Return: always 0 (success)
   */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	return (0);
 }
 	
