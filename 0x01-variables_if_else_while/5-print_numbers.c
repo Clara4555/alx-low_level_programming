19beaf37d7d4971a7662a82aca7f5c08e2849165
Clara4555 <favourloyalty8@gmail.com>
2023-04-21 17:07:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - Entry pass
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	int t;
+
+	for (t = 0; t <= 9; t++)
+	{
+		printf("%d", t);
+	}
+	printf("\n");
+	return (0);
+}
