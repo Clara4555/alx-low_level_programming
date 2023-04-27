7cefeb17f7a8e88ba2c0b61d29e8ee5ba1b2cb05
Clara4555 <favourloyalty8@gmail.com>
2023-04-27 14:47:26 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_numbers - prints number from 0 -9
+  *Return: always 0
+  */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; ++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
