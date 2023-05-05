dc4c82aa3c3a71334fec6514a2f36cf01a6a48fd
Clara4555 <favourloyalty8@gmail.com>
2023-05-05 10:02:58 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_number - function that prints integer
+  *@n: input
+  *Return: no return
+  */
+void print_number(int n)
+{
+	unsigned a ,b, counter;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		a =  n * - 1;
+	}
+	else
+	{
+		a = n;
+	}
+	b = a;
+	counter = 1;
+	while (b > 9)
+	{
+		b /= 10;
+		counter *= 10;
+	}
+	for (; counter >= 1; counter /= 10)
+	{
+		_putchar(((a / counter) % 10) + 48);
+	}
+}
