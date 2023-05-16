284b6356aad757edaa18b7bf041d9eda4667459b
Clara4555 <favourloyalty8@gmail.com>
2023-05-16 01:30:53 -0700

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,3 @@
 #include "main.h"
-#include <stdio.h>
-/**
-  *_puts - function that prints a string followed by a new line
-  *@str: string to print
-  *Return Always 0 (success)
-  */
 void _puts(char *str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-}
+{}
