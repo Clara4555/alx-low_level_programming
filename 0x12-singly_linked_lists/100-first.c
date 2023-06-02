b75ffa0fb2a1e813e515cc336c3add76f0777235
Clara4555 <favourloyalty8@gmail.com>
2023-06-01 18:11:36 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void print_message_attributor(void);
+/**
+  *print_message_attributor -function where the printing taskes place
+  *
+  */
+void print_message_attributor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
