0aa2c9496dec44b9102e5a5680f75b6a69b0f3ca
Clara4555 <favourloyalty8@gmail.com>
2023-06-05 15:22:55 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,11 +2,21 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
+  *listint_len - function that prints all the elements of a listint_t list
   *
+  *@h: nodes
   *
-  *
-  *
-  *
+  *Return: nodes
   *
   */
+size_t listint_len(const listint_t *h)
+{
+	size_t c = 0;
 
+	while (h)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
